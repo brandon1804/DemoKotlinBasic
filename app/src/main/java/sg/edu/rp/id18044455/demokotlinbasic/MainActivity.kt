a151a822b37db78bce8231b1f9fc4ff0a58bbf94
brandon1804 <18044455@rp.edu.sg>
2021-08-12 10:48:06 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/id18044455/demokotlinbasic/MainActivity.kt
@@ -0,0 +1,72 @@
+package sg.edu.rp.id18044455.demokotlinbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnGetAge.setOnClickListener {
+            val userDOB  = etDOB.text.toString()
+            if(userDOB.isNotEmpty()){
+                val year = Calendar.getInstance().get(Calendar.YEAR)
+                val age = year - userDOB.toInt();
+                tvShowAge.text = "Your age is $age"
+
+                var output = ""
+
+                /*
+                if(age < 18){
+                    output = "Underage"
+                }
+                else if (age in 18..65){
+                    output = "Young people"
+                }
+                else if(age < 66.79){
+                    output = "Middle-aged"
+                }
+                else if (age in 80..99){
+                    output = "Elderly"
+                }
+                else{
+                    output = "Long-lived elderly"
+                }
+                 */
+
+                when (age){
+                    in  0..17 -> output = "Underage"
+                    in  18..65 -> output = "Young people"
+                    in  66..79 -> output = "Middle-aged"
+                    in  80..99 -> output = "Elderly"
+                    else ->{
+                        output = "Long-lived elderly"
+                    }
+
+                }//end of when
+
+
+                tvAgeGroup.text = output
+
+
+            }//end of text field validation
+            else{
+                Toast.makeText(this, "Please enter your birth year", Toast.LENGTH_SHORT).show()
+            }
+
+        }//end of btnGetAge
+
+
+
+
+
+
+
+    }//end of onCreate
+}//end of class
